b3f8b9e628fce11eacb6d56f4a2f1eb1347e12be
KarimSIM2024 <kareemzwael@gmail.com>
2025-04-24 00:28:48 +0200

    fix signup page

--- scripts/pages/signup.js
+++ scripts/pages/signup.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
       form.style.display = "none";
       successMessage.style.display = "block";
       setTimeout(() => {
-        window.location.href = "../pages/dashboard.html";
+        window.location.href = "/dashboard.html";
       }, 2000);
     }
   });
