a6349b02a9261bd9f52100012747e1652b1bf3c6
mester3lwa19 <tomaskhaled196@gmail.com>
2025-04-25 18:00:39 +0300

    Solved the logo icon in sign up , login page, finished language selection page, updated footer icon with fontawesome

--- scripts/pages/leaderboard.js
+++ scripts/pages/leaderboard.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Mobile menu toggle
-  const menuToggle = document.getElementById("hamburger");
-  const sidebar = document.getElementById("sidebar");
 
-  menuToggle.addEventListener("click", function () {
-    sidebar.classList.toggle("active");
-  });
 
   // leaderboard.js
   const leaderboard = [
