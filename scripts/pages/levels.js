93e92374aedc29888678162753f806c6f891d05f
mester3lwa19 <tomaskhaled196@gmail.com>
2025-04-26 22:19:56 +0300

    trying to solve level path error 2

--- scripts/pages/levels.js
+++ scripts/pages/levels.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // In a real app, this would navigate to the lesson
         // alert(`Starting Level ${level.number}: ${level.title}`);
-        window.location.href = 'level.html';
+        window.location.href = '/Prolingo/level.html';
       });
 
       levelsGrid.appendChild(levelCard);
